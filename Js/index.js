b1ac5c78200d28f539ecc94093f0d6ba35abbe8c
Al Shaharia <ifty682002@gmail.com>
2022-09-03 16:55:03 +0600

    category button worked

--- Js/index.js
+++ Js/index.js
@@ -8,17 +8,23 @@ callCategory();
 
 const showCategory = (data) => {
   data.forEach((categoryData) => {
-    const { category_name } = categoryData;
-    console.log(categoryData);
+    const { category_name, category_id } = categoryData;
     const category = document.getElementById("category");
     const li = document.createElement("li");
-    // ul.classList.add("menu");
-    // ul.classList.add("menu-horizontal");
-    // ul.classList.add("p-0");
+    li.classList.add("text-xl");
 
-    li.innerText = `
-    ${category_name}
+    li.innerHTML = `
+    <div id="${category_id}" onclick="newsData(this)">${category_name}</div>
       `;
     category.appendChild(li);
   });
+};
+
+const newsData = async (element) => {
+  console.log(element.id);
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${element.id}`
+  );
+  const data = await response.json();
+  return data;
 };
