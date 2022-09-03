db539054f2423dc3e42f86dcb23795a51aa2c0d8
Al Shaharia <ifty682002@gmail.com>
2022-09-03 19:31:54 +0600

    add spinner successfully

--- Js/index.js
+++ Js/index.js
@@ -22,16 +22,24 @@ const showCategory = (data) => {
 
 const newsData = async (element) => {
   //   console.log(element.id);
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("hidden");
+  spinner.classList.add("flex");
   fetch(`https://openapi.programming-hero.com/api/news/category/${element.id}`)
     .then((res) => res.json())
     .then((data) => newsDetails(data.data, element));
 };
 
 const newsDetails = (data, element) => {
-  console.log(data);
+  //   console.log(data);
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("flex");
+  spinner.classList.add("hidden");
   const categoryLength = document.getElementById("category-length");
   categoryLength.innerHTML = `
-    <h1 class="p-5 text-xl">${data.length} items found for category ${element.innerText}</h1>
+    <h1 class="p-5 text-xl">${
+      data.length === 0 ? "No" : data.length
+    } items found for category ${element.innerText}</h1>
     `;
   const newsCart = document.getElementById("news-cart");
   newsCart.innerHTML = ``;
@@ -49,7 +57,7 @@ const newsDetails = (data, element) => {
                       ? news.details.slice(0, 300) + "..."
                       : news.details
                   }</p>
-                  <div class="card-actions flex justify-between ">
+                  <div class="card-actions flex justify-between items-center">
                   <div class="flex items-center">
                     <img src=${
                       news.author.img
@@ -61,12 +69,14 @@ const newsDetails = (data, element) => {
                     }</h1>
                   </div>
                   <div class="flex items-center">
-                  <i class="fa-solid fa-eye mx-3"></i>
-                  <p>${
-                    news.total_view === null ? "No data found" : news.total_view
-                  }</p>
+                    <i class="fa-solid fa-eye mx-3"></i>
+                    <p>${
+                      news.total_view === null
+                        ? "No data found"
+                        : news.total_view
+                    }</p>
                   </div>
-                    <button class="btn btn-primary">Read More</button>
+                    <label for="my-modal-3" class="btn modal-button">Read More</label>
                   </div>
                 </div>
               </div>
